8aa10b1157cb0855dbb20af2c8263fb80451f46d
Steve Castle <steve@tigerhaunt.com>
2019-05-07 17:02:28 -0700

    add menu items

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'gatsby';
 import pn from '../img/pn.svg';
+import gallery from '../img/gallery.svg';
+import sunkware from '../img/sunkware.svg';
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
@@ -10,6 +12,12 @@ const Navbar = () => (
         <Link to="/" className="navbar-item" title="Home">
           <img src={pn} alt="PunkNaturalism" style={{ width: '34px' }} />
         </Link>
+        <Link to="/gallery" className="navbar-item" title="Gallery">
+          <img src={gallery} alt="Photo gallery" style={{ width: '34px' }} />
+        </Link>
+        <a href="https://sunkware.com" className="navbar-item" title="Sunkware">
+          <img src={sunkware} alt="SunkWare: A Tech Blog" style={{ width: '34px' }} />
+        </a>
       </div>
       </div>
     </div>
